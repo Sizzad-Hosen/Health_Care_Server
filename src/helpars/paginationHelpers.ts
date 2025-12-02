56361fcdc0683ff0a4c0a849fa531d4ab6426d10
Sizzad Hosen <sizzadhosenofficial@gmail.com>
2025-12-02 06:59:13 +0600

    doctor post , get , update route , controller , service create

--- src/helpars/paginationHelpers.ts
+++ src/helpars/paginationHelpers.ts
@@ -1,12 +1,26 @@
-const calculatePagination = (options:{
-    page?:number,limit?:number,sortBy?:string,sortOrder?:string
-})=>{
-    const page = Number(options.page) || 1;
-    const limit = Number(options.limit)||10;
-    const skip = (Number(page)-1)*limit;
-const sortBy:string = options.sortBy || 'createdAt'
-const sortOrder = options.sortOrder || 'desc'
+type IOptions = {
+    page?: number,
+    limit?: number,
+    sortOrder?: string,
+    sortBy?: string
+}
 
+type IOptionsResult = {
+    page: number,
+    limit: number,
+    skip: number,
+    sortBy: string,
+    sortOrder: string
+}
+
+const calculatePagination = (options: IOptions): IOptionsResult => {
+
+    const page: number = Number(options.page) || 1;
+    const limit: number = Number(options.limit) || 10;
+    const skip: number = (Number(page) - 1) * limit;
+
+    const sortBy: string = options.sortBy || 'createdAt';
+    const sortOrder: string = options.sortOrder || 'desc';
 
     return {
         page,
@@ -16,6 +30,8 @@ const sortOrder = options.sortOrder || 'desc'
         sortOrder
     }
 }
-export const pagination = {
+
+
+export const paginationHelper = {
     calculatePagination
 }
